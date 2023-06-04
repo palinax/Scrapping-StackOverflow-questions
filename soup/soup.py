46fff86b34e64e88cc2f099d21a321bba8a2b577
Palinax <45271080+palinax@users.noreply.github.com>
2023-06-04 17:57:53 +0200

    Update soup.py

--- soup/soup.py
+++ soup/soup.py
@@ -3,7 +3,9 @@ from bs4 import BeautifulSoup
 import time
 import csv
 import pandas as pd
-import sys
+
+# Boolean parameter to limit the number of pages
+limit_pages = True
 
 # Set up the session and user agent
 base_url = "https://stackoverflow.com"
@@ -16,21 +18,51 @@ page_count = 0  # Counter for the number of pages
 
 start_time = time.time()  # Start timestamp
 
-# Get the command line arguments
-limit_pages = True
-
 # Iterate through the range of page numbers
 for page_num in range(1, 101):  # Start from 1, go up to 100
     if limit_pages and page_count >= 100:
         break
 
-    # Rest of the code remains the same
+    # Construct the URL for the page
+    url = f"https://stackoverflow.com/questions/tagged/web-scraping?tab=votes&page={page_num}&pagesize=15"
 
-    question_counter += 1  # Increment the question counter
-    page_count += 1  # Increment the page counter
+    # Send the request and get the response
+    response = s.get(url)
 
-    if question_counter >= 15:  # Stop scraping if the question count reaches 15
-        break
+    # Parse the HTML content using BeautifulSoup
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # Find all the question elements on the page
+    questions = soup.find_all('div', class_='s-post-summary')
+
+    question_counter = 0  # Counter for the number of questions
+
+    # Iterate through each question element
+    for question in questions:
+        # Extract the question title, link, tags, votes, answers, and views
+        q_title = question.find('a', class_='s-link').text.strip()
+        q_link = base_url + question.find('a', class_='s-link')['href']
+        q_tags = [tag.text for tag in question.find_all('a', class_='post-tag')]
+        q_votes = question.select_one('.s-post-summary--stats-item__emphasized span').text
+        q_answers_span = question.select_one('.s-post-summary--stats-item.has-answers span')
+        q_answers = q_answers_span.text if q_answers_span else 'N/A'
+        q_views_span = question.select_one('.s-post-summary--stats-item:nth-child(3) span')
+        q_views = q_views_span.text if q_views_span else 'N/A'
+
+        # Add the question information to the list
+        questions_info.append({
+            'Title': q_title,
+            'Link': q_link,
+            'Tags': q_tags,
+            'Votes': q_votes,
+            'Views': q_views,
+            'Answers': q_answers,
+        })
+
+        question_counter += 1  # Increment the question counter
+
+        if question_counter >= 15:  # Stop scraping if the question count reaches 15
+            break
 
     time.sleep(1)  # Pause for 1 second before making the next request
 
@@ -48,4 +80,6 @@ print(f"Total elapsed time: {elapsed_time_minutes} minutes")
 
 # Convert the list of question information to a DataFrame
 df = pd.DataFrame(questions_info)
-df.head()
+
+# Save the DataFrame to a CSV file
+df.to_csv('questions_output.csv', index=False)
